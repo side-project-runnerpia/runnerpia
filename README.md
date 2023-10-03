f06c69c2287e04c456d10fb76ef4d02cabb50bec
zwundzwzig <amoeba8282@gmail.com>
2023-10-03 18:33:57 +0900

    [feat] 개발 초기 세팅

---
+++ README.md
@@ -0,0 +1,13 @@
+# runnerpia-backend
+사이드 프로젝트(러너피아) 백엔드 repo
+
+<br>
+
+### 개발환경
+- Java17
+- SpringBoot 3.1.4
+- Gradle
+- Junit5
+- MaraiDB(RDS)
+- S3
+
---
+++ src/main/java/com/runnerpia/boot/RunnerpiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.runnerpia.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerpiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnerpiaBackendApplication.class, args);
+	}
+
+}
