5171e1326be833e37d4e17c5a85b6869c270cc8f
Hambak-note <dudghks5722@gmail.com>
2023-10-05 23:34:34 +0900

    [feat] : S3, RDS 연동

---
+++ src/test/java/com/runnerpia/boot/s3/S3ConnectionTest.java
@@ -0,0 +1,20 @@
+package com.runnerpia.boot.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class S3ConnectionTest {
+
+    @Autowired
+    private AmazonS3 amazonS3;
+
+    @Test
+    public void s3ConnectionTest() {
+        assertThat(amazonS3.listBuckets()).isNotNull();
+    }
+}
