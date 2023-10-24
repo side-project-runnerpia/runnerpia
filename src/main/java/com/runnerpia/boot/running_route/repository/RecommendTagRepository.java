7a486e1d2f9114d8862bc8623c3713de1cfba59b
zwundzwzig <amoeba8282@gmail.com>
2023-10-24 13:15:33 +0900

    [feat] 실시간 인기 태그

--- src/main/java/com/runnerpia/boot/running_route/repository/RecommendTagRepository.java
+++ src/main/java/com/runnerpia/boot/running_route/repository/RecommendTagRepository.java
@@ -6,15 +6,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RecommendTagRepository extends JpaRepository<RecommendTag, UUID> {
-  @Query("SELECT NEW com.runnerpia.boot.running_route.dto.TagRecordResponseDto(t.description, COUNT(s.tag)) " +
+  @Query("SELECT NEW com.runnerpia.boot.running_route.dto.TagRecordResponseDto(t.description, COUNT(rt.tag)) " +
           "FROM Tag t " +
-          "LEFT JOIN RecommendTag s ON t.id = s.tag.id " +
-          "LEFT JOIN RunningRoute r ON s.runningRoute.id = r.id " +
-          "WHERE r.id = :routeId " +
+          "LEFT JOIN RecommendTag rt ON t.id = rt.tag.id " +
+          "LEFT JOIN RunningRoute r ON rt.runningRoute.id = r.id " +
+          "WHERE rt.id = :routeId " +
           "AND t.id = :tagId " +
           "GROUP BY t.description")
   TagRecordResponseDto countTagsByRoute(@Param("routeId") UUID routeId, @Param("tagId") UUID tagId);
+
+  @Query("SELECT NEW com.runnerpia.boot.running_route.dto.TagRecordResponseDto(t.description, COUNT(rt.tag)) " +
+          "FROM Tag t " +
+          "JOIN RecommendTag rt ON t.id = rt.tag.id " +
+          "GROUP BY t.id, t.description " +
+          "ORDER BY COUNT(rt.tag) DESC")
+  List<TagRecordResponseDto> getPopularTags();
 }
